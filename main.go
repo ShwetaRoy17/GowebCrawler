6d85f9c9e7e114a234501ac1be1e7de369fd30c1
ShwetaRoy17 <royshweta1712@gmail.com>
2026-03-24 01:33:56 +0530

    completed parsing function

--- main.go
+++ main.go
@@ -2,20 +2,56 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+	"sync"
+	"time"
+
 	"github.com/ShwetaRoy17/GowebCrawler/fetcher"
 	"github.com/ShwetaRoy17/GowebCrawler/parser"
 )
 
-func main() {
-	body, err := fetcher.Fetch("https://www.google.com")
+func h1(urlString string){
+		fmt.Println(urlString,time.Now())
+
+	body, err := fetcher.Fetch(urlString)
 	if err != nil {
 		fmt.Println("Error fetching the data:", err.Error())
 		return
 	}
-	runes := []rune(body)
-	if len(runes) > 500 {
-		runes = runes[:500]
+	parsedUrl,err := url.Parse(urlString)
+	
+	abc,err := parser.Parse(parsedUrl,body)
+	if err != nil {
+		fmt.Println("Error parsing the data:", err.Error())
+		return
 	}
-	fmt.Println(string(runes))
-	parser.Parse()
+	fmt.Printf("Found %d links\n", abc)
+	fmt.Println(urlString,time.Now())
+
+}
+
+func main() {
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.twitter.com",
+		"https://www.linkedin.com",
+		"https://www.github.com",
+	}
+
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			h1(u)
+		}(url)
+	}
+	wg.Wait()
+
+	// runes := []rune(body)
+	// if len(runes) > 500 {
+	// 	runes = runes[:500]
+	// }
+	// fmt.Println(string(runes))
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,50 +1,48 @@
 package parser
 
 import (
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
 
-func Parse() {
-	html.Parse(nil)
-}
-
-
-
 
 type Link struct {
-	URL string
+	URL  string
 	Text string
 }
 
-func extractLinks(body string)([]Link, error ){
+func Parse(base *url.URL, body string) ([]Link, error) {
 	doc, err := html.Parse(strings.NewReader(body))
-	if err!= nil {
+	if err != nil {
 		return nil, err
 	}
 	var links []Link
 
 	var walk func(*html.Node)
-	walk = func(n *html.Node){
+	walk = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			var href string
 
 			for _, a := range n.Attr {
-				if a.Key == "href"{
+				if a.Key == "href" {
 					href = a.Val
 					break
 				}
 			}
 			if href != "" {
-				links = append(links, Link{
-					URL: href,
-					Text: strings.TrimSpace(nodeText(n)),
-				})
+				if absURL := resolveURL(base, href); absURL != "" {
+
+					links = append(links, Link{
+						URL:  href,
+						Text: strings.TrimSpace(nodeText(n)),
+					})
+				}
 			}
 		}
-		for c:= n.FirstChild; c != nil; c = c.NextSibling {
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			walk(c)
 		}
 	}
@@ -57,8 +55,30 @@ func nodeText(n *html.Node) string {
 		return n.Data
 	}
 	var b strings.Builder
-	for c:= n.FirstChild; c!= nil ; c = c.NextSibling {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		b.WriteString(nodeText(c))
 	}
 	return b.String()
+}
+
+func resolveURL(base *url.URL, href string) string {
+	ref, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	resolved := base.ResolveReference(ref).String()
+	if checkValidUrl(resolved) {
+		return resolved
+	}
+	return ""
+}
+
+func checkValidUrl(url string) bool {
+	if strings.HasPrefix(url, "mailto:") || strings.HasPrefix(url, "tel:") || strings.HasPrefix(url,"data:") || strings.HasPrefix(url,"ftp:") {
+		return false
+	}
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		return false
+	}
+	return true
 }
